6745c413e855af8b750440449e2791c5cbd5aaf4
Yonathan K <ykebede2@umbc.edu>
2020-06-19 02:07:24 -0400

    CS50 Intro to AI - complete lecture code

--- CS50_intro_to_ai/lecture_code/Uncertainty/inference.py
+++ CS50_intro_to_ai/lecture_code/Uncertainty/inference.py
@@ -1,15 +1,16 @@
 from model import model
 
-# Calculate predictions
+# Insert evidence that train is delayed (Observed evidence)
 predictions = model.predict_proba({
     "train": "delayed"
 })
 
-# Print predictions for each node
+# calculate the probability for each event
 for node, prediction in zip(model.states, predictions):
     if isinstance(prediction, str):
         print(f"{node.name}: {prediction}")
     else:
         print(f"{node.name}")
         for value, probability in prediction.parameters[0].items():
-            print(f"    {value}: {probability:.4f}")
+            print(f"\t{value}: ", end='')
+            print("{:.4f}".format(probability))
--- CS50_intro_to_ai/lecture_code/Uncertainty/model.py
+++ CS50_intro_to_ai/lecture_code/Uncertainty/model.py
@@ -57,27 +57,3 @@ model.add_edge(train, appointment)
 
 # Finalize model
 model.bake()
-
-# calculate probability for a given observation
-probability = model.probability([["none", "no", "on time", "attend"]])
-
-print("P(rain=none, maintenance=no, train=on time, appointment=attend)",
-    probability, end="\n\n")
-
-# Insert evidence that train is delayed (Observed evidence)
-predictions = model.predict_proba({
-    "train": "delayed"
-})
-
-# calculate the probability for each event
-for node, prediction in zip(model.states, predictions):
-    if isinstance(prediction, str):
-        print(f"{node.name}: {prediction}")
-    else:
-        print(f"{node.name}")
-        for value, probability in prediction.parameters[0].items():
-            print(f"\t{value}: ", end='')
-            print("{:.4f}".format(probability))
-
-
-
